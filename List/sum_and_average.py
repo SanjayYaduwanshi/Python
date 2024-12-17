02515080d0147dc8c969d381ac2edca86cf0194f
Sanjay Yaduwanshi <sanjay.yaduwanshi@mindpathtech.com>
2024-12-17 18:18:26 +0530

    sum and average

--- List/sum_and_average.py
+++ List/sum_and_average.py
@@ -6,9 +6,15 @@ nums = [11, 2,3,5,4,6,7,8,9,2,1,2, 3, 4, 5, 61, 7, 8, 9, 101,11]
 
 
 #Average
-avg=0
-sum=0
-for i in nums:
-    sum+=i
-avg=round(sum/len(nums),2)
-print(avg)
+# avg=0
+# sum=0
+# for i in nums:
+#     sum+=i
+# avg=round(sum/len(nums),2)
+# print(avg)
+
+
+# using sum() method
+total=sum(nums)
+print("sum=",total)
+print("average= ",round(total/len(nums),2))
