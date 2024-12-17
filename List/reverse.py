787780867458a12acaee327bf96f9cfee298d214
Sanjay Yaduwanshi <sanjay.yaduwanshi@mindpathtech.com>
2024-12-17 14:57:49 +0530

    optimized version

--- List/reverse.py
+++ List/reverse.py
@@ -7,3 +7,18 @@ for i in range(int(length/2)):
     nums[length-i-1]=nums[i]
     nums[i]=temp
 print(nums)    
+
+# using reverse method
+nums.reverse()
+print(nums)
+
+
+#optimized code
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+length = len(nums)
+
+# Reverse the list manually using a for loop
+for i in range(length // 2):
+    nums[i], nums[length - i - 1] = nums[length - i - 1], nums[i]
+
+print(nums)
